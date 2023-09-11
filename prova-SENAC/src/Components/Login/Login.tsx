2b7103bf99fe4129a897f8fc9734c8811e48ea24
rodolfo.bortoluzzi <rodolfo.bortoluzzi@bit.com.br>
2023-09-11 17:39:52 -0300

    aula 1109

---
+++ prova-SENAC/src/Components/Login/Login.tsx
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+
+interface LoginProps {
+  onLogin: (username: string, password: string) => void;
+}
+
+const Login: React.FC<LoginProps> = ({ onLogin }) => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = () => {    
+    if (username === "usuario" && password === "senha") {
+      onLogin(username, password);
+    } else {
+      alert("Credenciais inválidas. Tente novamente.");
+    }
+  };
+
+  return (
+    <div>      
+      <div>
+        <label>Usuário:</label>
+        <input
+          type="text"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+      </div>
+      <div>
+        <label>Senha:</label>
+        <input
+          type="password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+      </div>
+      <button onClick={handleLogin}>Entrar</button>
+    </div>
+  );
+};
+
+export default Login;
